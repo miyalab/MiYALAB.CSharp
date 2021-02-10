95dbf3796b46cdc19f8f5c146ae944b9d4cc4492
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2021-02-10 10:56:19 +0900

    2021.02.10 update

---
+++ LibCheck/FormMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MiYALAB.CSharp.Device;
+
+namespace LibCheck
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonCheck_Click(object sender, EventArgs e)
+		{
+			foreach(Device device in SerialDeviceProcessor.GetDeviceList())
+			{
+				textBoxLog.AppendText(device.name + Environment.NewLine);
+				textBoxLog.AppendText(device.pnpClass + Environment.NewLine);
+				textBoxLog.AppendText(device.deviceId + Environment.NewLine);
+				textBoxLog.AppendText(Environment.NewLine);
+
+			}
+		}
+	}
+}
---
+++ MiYALAB.CSharp/DeviceProcessor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiYALAB.CSharp.Device
+{
+	/// <summary>
+	/// デバイスデータ
+	/// </summary>
+	public struct DeviceProperty
+	{
+		/// <summary>
+		/// ハードウェア名
+		/// </summary>
+		public string name;
+		/// <summary>
+		/// pnpクラス
+		/// </summary>
+		public string pnpClass;
+		/// <summary>
+		/// ハードウェアID
+		/// </summary>
+		public string deviceId;
+	}
+
+	/// <summary>
+	/// キャプチャデバイスクラス
+	/// </summary>
+	public class CaptureDeviceProcessor
+	{
+		/// <summary>
+		/// キャプチャデバイスリスト取得
+		/// </summary>
+		/// <returns>キャプチャデバイスリスト</returns>
+		public static DeviceProperty[] GetDeviceList()
+		{
+			DeviceProperty deviceWork;
+			List<DeviceProperty> devices = new List<DeviceProperty>();
+
+			foreach(ManagementObject device in new ManagementClass("Win32_PnPEntity").GetInstances().Cast<ManagementObject>())
+			{
+				deviceWork.pnpClass = (string)device.GetPropertyValue("PNPClass");
+				if (deviceWork.pnpClass == "Image" || (string)deviceWork.pnpClass == "Camera")
+				{
+					deviceWork.name = (string)device.GetPropertyValue("Name");
+					deviceWork.deviceId = (string)device.GetPropertyValue("DeviceID");
+					devices.Add(deviceWork);
+				}
+			}
+
+			return devices.ToArray();
+		}
+	}
+
+	/// <summary>
+	/// シリアル通信デバイスクラス
+	/// </summary>
+	public class SerialDeviceProcessor
+	{
+		/// <summary>
+		/// シリアル通信デバイスリスト取得
+		/// </summary>
+		/// <returns>シリアル通信デバイスリスト</returns>
+		public static DeviceProperty[] GetDeviceList()
+		{
+			DeviceProperty deviceWork;
+			List<DeviceProperty> devices = new List<DeviceProperty>();
+
+			foreach (ManagementObject device in new ManagementClass("Win32_PnPEntity").GetInstances().Cast<ManagementObject>())
+			{
+				deviceWork.pnpClass = (string)device.GetPropertyValue("PNPClass");
+				if (deviceWork.pnpClass == "Ports")
+				{
+					deviceWork.name = (string)device.GetPropertyValue("Name");
+					deviceWork.deviceId = (string)device.GetPropertyValue("DeviceID");
+					devices.Add(deviceWork);
+				}
+			}
+
+			return devices.ToArray();
+		}
+	}
+}
