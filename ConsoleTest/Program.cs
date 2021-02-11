fa1164f0d3cbe8f7926c70ad85608baf96d65966
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2021-02-12 08:36:48 +0900

    2021.02.12 update

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
--- MiYALAB.CSharp/DeviceProcessor.cs
+++ MiYALAB.CSharp/DeviceProcessor.cs
@@ -1,86 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Management;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MiYALAB.CSharp.Device
 {
-	/// <summary>
-	/// デバイスプロパティデータ
-	/// </summary>
-	public struct DeviceProperty
-	{
+    /// <summary>
+    /// デバイス情報データ
+    /// </summary>
+    public struct DeviceInfo
+    {
 		/// <summary>
 		/// ハードウェア名
 		/// </summary>
-		public string name;
+        public string name;
 		/// <summary>
 		/// pnpクラス
 		/// </summary>
-		public string pnpClass;
-		/// <summary>
-		/// ハードウェアID
-		/// </summary>
-		public string deviceId;
-	}
+        public string pnpClass;
+        public string hardwareId;
+    }
 
-	/// <summary>
-	/// キャプチャデバイスクラス
-	/// </summary>
-	public class CaptureDeviceProcessor
-	{
-		/// <summary>
-		/// キャプチャデバイスリスト取得
-		/// </summary>
-		/// <returns>キャプチャデバイスリスト</returns>
-		public static DeviceProperty[] GetDeviceList()
-		{
-			DeviceProperty deviceWork;
-			List<DeviceProperty> devices = new List<DeviceProperty>();
+    /// <summary>
+    /// キャプチャデバイスデータ取得
+    /// </summary>
+    public class CaptureDeviceProcessor
+    {
+        public static List<DeviceInfo> GetCaptureDeviceList()
+        {
+            DeviceInfo work;
+            List<DeviceInfo> devices = new List<DeviceInfo>();
+            ManagementClass pnpEntity = new ManagementClass("Win32_PnPEntity");
 
-			foreach(ManagementObject device in new ManagementClass("Win32_PnPEntity").GetInstances().Cast<ManagementObject>())
-			{
-				deviceWork.pnpClass = (string)device.GetPropertyValue("PNPClass");
-				if (deviceWork.pnpClass == "Image" || deviceWork.pnpClass == "Camera")
-				{
-					deviceWork.name = (string)device.GetPropertyValue("Name");
-					deviceWork.deviceId = (string)device.GetPropertyValue("DeviceID");
-					devices.Add(deviceWork);
-				}
-			}
+            foreach(var pnp in pnpEntity.GetInstances())
+            {
+                if ((string)pnp.GetPropertyValue("PNPClass") == "Camera")
+                {
+                    work.name = pnp.GetPropertyValue("name").ToString();
+                    work.pnpClass = "Camera";
+                    work.hardwareId = pnp.GetPropertyValue("HardwareID").ToString();
 
-			return devices.ToArray();
-		}
-	}
+                    devices.Add(work);
+                }
+            }
 
-	/// <summary>
-	/// シリアル通信デバイスクラス
-	/// </summary>
-	public class SerialDeviceProcessor
-	{
-		/// <summary>
-		/// シリアル通信デバイスリスト取得
-		/// </summary>
-		/// <returns>シリアル通信デバイスリスト</returns>
-		public static DeviceProperty[] GetDeviceList()
-		{
-			DeviceProperty deviceWork;
-			List<DeviceProperty> devices = new List<DeviceProperty>();
+            return devices;
+        }
+    }
 
-			foreach (ManagementObject device in new ManagementClass("Win32_PnPEntity").GetInstances().Cast<ManagementObject>())
-			{
-				deviceWork.pnpClass = (string)device.GetPropertyValue("PNPClass");
-				if (deviceWork.pnpClass == "Ports")
-				{
-					deviceWork.name = (string)device.GetPropertyValue("Name");
-					deviceWork.deviceId = (string)device.GetPropertyValue("DeviceID");
-					devices.Add(deviceWork);
-				}
-			}
+    /// <summary>
+    /// シリアル通信デバイスデータ取得
+    /// </summary>
+    public class SerialDeviceProcessor
+    {
+        public static IEnumerable<string> GetSerialDeviceList()
+        {
+            ManagementClass pnpEntity = new ManagementClass("Win32_PnPEntity");
+            Regex comRegex = new Regex(@"\(COM[1-9][0-9]?[0-9]?\)");               // COMとつくものを探索
 
-			return devices.ToArray();
-		}
-	}
+            return pnpEntity
+                .GetInstances()                                                     // 一覧を取得
+                .Cast<ManagementObject>()
+                .Select(managementObj => managementObj.GetPropertyValue("Name"))    // デバイス名取得
+                .Where(nameObj => nameObj != null)                                  // nullのものは除外
+                .Select(nameObj => nameObj.ToString())                              // 文字列に変換
+                .Where(name => comRegex.IsMatch(name));                             // 正規表現でフィルタリング
+        }
+    }
 }
